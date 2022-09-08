e36131cbd1af684eb4d9c96e5b562cf0f607a8e0
Armando <armando.zabala@mercadolibre.com.co>
2022-09-08 13:12:21 -0500

    string funciones reverse, split, replace and replace all

--- 15-string/main.go
+++ 15-string/main.go
@@ -5,16 +5,37 @@ import (
 	"strings"
 )
 
-func esPalindromo(palabra string) {
+func reverse(palabra string) string {
+	arrayCadena := strings.Split(palabra, "")
+	arrayInvertida := make([]string, 0)
+
+	for i := len(arrayCadena) - 1; i >= 0; i-- {
+		arrayInvertida = append(arrayInvertida, arrayCadena[i])
+	}
+
+	//convierte en string
+	return strings.Join(arrayInvertida, "")
+}
+
+func esPalindromo(palabra string) bool {
+	//opalabra original
 	fmt.Println(palabra)
 	//palabra to Lower
 	palabra = strings.ToLower(palabra)
+	palabra = strings.Replace(palabra, "z", "s", 2)
+	palabra = strings.ReplaceAll(palabra, " ", "")
 
-	fmt.Println(palabra)
+	palabraInvertida := reverse(palabra)
+
+	return palabra == palabraInvertida
 }
 
 func main() {
 
-	esPalindromo("Oso")
+	if esPalindromo("Luz") {
+		fmt.Println("Es Palindrome")
+	} else {
+		fmt.Println("No Es Palindrome")
+	}
 
 }
---
+++ 16-calculadora/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var expresion string
+	var result int
+
+	fmt.Print("=>")
+	fmt.Scanln(&expresion)
+}
